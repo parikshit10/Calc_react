eb23fdfba8b77b1c7b5b78d23fc8e77110a9dec2
Parikshit <parikshit@Parikshits-MacBook-Air.local>
2019-05-12 00:10:27 +0530

    added handler

--- src/App.js
+++ src/App.js
@@ -3,20 +3,43 @@ import Button from './Button'
 import 'tachyons'
 class App extends React.Component
 {
+  constructor()
+  {
+    super();
+    this.state = 
+    {
+      exp: ''
+    }
+    this.handler = this.handler.bind(this)
+  }
+
+  handler = (changedVal) =>
+  {
+    this.setState({exp: this.state.exp+changedVal})
+    console.log(this.state.exp)
+  }
   render()
   {
     var allButtons = [];
-    for(var i = 1; i <= 7; i+=3)
+    var lst = [];
+    for(var i = 1; i <= 9; i+=1)
     {
-
-      allButtons.push(<Button start={i} end ={i+3}/>);
+      
+      lst.push(i.toString())
+      if(lst.length === 3)
+      {
+        allButtons.push(<Button handler={this.handler} items={lst}/>);
+        lst = [];
+      }
     }
-    allButtons.push(<Button start={0} end ={1}/>)
+    lst = ["0",".","="]
+    allButtons.push(<Button handler={this.handler} items={lst}/>)
 
     return (
     <div>
       <h1> Basic Calculator app </h1>
       {allButtons}
+
     </div>
     );  
   }
