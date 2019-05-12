b049d2669ec9e9a639402d16efd73751e749c939
Parikshit <parikshit@Parikshits-MacBook-Air.local>
2019-05-12 13:45:17 +0530

    all keys working

--- src/Button.js
+++ src/Button.js
@@ -11,6 +11,14 @@ const Button = (props) =>
 		return <button key={ind} onClick = {() => {handleToUpdate(ind)}} onKeyDown ={(event) => {
 			if(event.keyCode <= 57 && event.keyCode >= 48)
 				handleToUpdate((event.keyCode-48).toString())
+			if(event.keyCode === 8 || event.keyCode == 27)
+				handleToUpdate("AC")
+			if(event.keyCode === 189)
+				handleToUpdate("-")
+			if(event.keyCode === 187 || event.keyCode === 13)
+				handleToUpdate("=")
+			if(event.keyCode === 191)
+				handleToUpdate("/")
 		}} className="f3 bw1 grow no-underline br-pill ph3 pv2 mb2 dib white bg-light-purple" type="button">{ind}</button>
 	});
 
